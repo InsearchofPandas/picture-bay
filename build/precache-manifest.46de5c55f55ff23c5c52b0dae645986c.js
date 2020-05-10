ff21391a1ae89bf1ba7f3654561bc53f9fe0de03
InsearchofPandas <kylerhumphrey@gmail.com>
2020-05-10 15:56:09 -0700

    add build

---
+++ build/precache-manifest.46de5c55f55ff23c5c52b0dae645986c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8390a4b98386b8e1737e61278fc95a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc8955025675fbe52f2d",
+    "url": "/static/css/main.85f99d34.chunk.css"
+  },
+  {
+    "revision": "de37518a568a06415687",
+    "url": "/static/js/2.fd58d42f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd58d42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc8955025675fbe52f2d",
+    "url": "/static/js/main.ea2242c2.chunk.js"
+  },
+  {
+    "revision": "0936d711013d31c1b036",
+    "url": "/static/js/runtime-main.a69d5c87.js"
+  }
+]);
